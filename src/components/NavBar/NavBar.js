21161350fff28d3ca37adacf6f87d87209295571
sabbir <sabbir@gmail.com>
2022-10-25 21:49:51 +0600

    navbar added

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { FcIdea } from 'react-icons/fc';
+import { Link } from 'react-router-dom';
+import './NavBar.module.css';
+
+const NavBar = () => {
+    return (
+
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <FcIdea className='h-8' />
+                    Learn-Programming-Languages</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Link to='/' > Home</Link>
+                        <Link to='/' > Courses</Link>
+                        <Link to='/' > FAQ</Link>
+                        <Link to='/' > Blog</Link>
+                        <Link to='/' > Login</Link>
+                        <Link to='/' > Register</Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">More deets</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            Dank memes
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
+    );
+};
+
+export default NavBar;

ac0f739386141b2fda25fbe7711950f710f757c7
sabbir <sabbir@gmail.com>
2022-10-26 19:18:26 +0600

    set spinners

--- src/Contexts/AuthProvider/AuthProvider.js
+++ src/Contexts/AuthProvider/AuthProvider.js
@@ -12,33 +12,36 @@ export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
 
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     //sign in with Google
     const signWithGoogle = (provider) => {
+        setLoading(true);
         return signInWithPopup(auth, provider);
     };
 
     //crate user by email and password
     const createUserWithEmail = (email, password) => {
-        // setLoading(true);
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     };
 
     //upadte user profile
     const updateUserProfile = (profile) => {
+        setLoading(true);
         return updateProfile(auth.currentUser, profile);
     };
 
 
     //login with email
     const loginWithEmail = (email, password) => {
-        // setLoading(true);
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     };
 
     //log out
     const logOut = () => {
-        // setLoading(true);
+        setLoading(true);
         return signOut(auth);
     };
 
@@ -46,8 +49,7 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
-
-
+            setLoading(false);
         });
 
         return () => unsubscribe();
@@ -60,7 +62,8 @@ const AuthProvider = ({ children }) => {
         loginWithEmail,
         signWithGoogle,
         user,
-        logOut
+        logOut,
+        loading
     };
 
     return (
--- src/Routes/PrivateRoute.js
+++ src/Routes/PrivateRoute.js
@@ -1,10 +1,17 @@
 import React, { useContext } from 'react';
+import { Spinner } from 'react-bootstrap';
 import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../Contexts/AuthProvider/AuthProvider';
 
 const PrivateRoute = ({ children }) => {
 
-    const { user } = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
+
+    if (loading) {
+        return <div className='text-center mt-5'>
+            <Spinner animation="border" variant="primary" />
+        </div>;
+    }
 
     if (!user) {
         return <Navigate to='/login' />;
